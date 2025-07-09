3e435eec4b1cf38d1f0faa639c55edb5c3ca0156
wieslekbaranowski <wieslaw.baranowski@intive.com>
2025-07-09 22:28:41 +0200

    cleanup

--- src/dto/flag_dto.go
+++ src/dto/flag_dto.go
@@ -14,9 +14,9 @@ type FlagDTO struct {
 func NewFlagDTO() *FlagDTO {
 	saveAll := flag.Bool("saveAll", true, "save all tenders to excel")
 	//max 1048567/12=87381, found 1090*12 =>1000
-	tenderPages := flag.Int("tenderPages", 3, "number of tender Pages to get")
+	tenderPages := flag.Int("tenderPages", 1000, "number of tender Pages to get")
 	//max 1000/50=200
-	orderPages := flag.Int("orderPages", 3, "number of order Pages to get")
+	orderPages := flag.Int("orderPages", 200, "number of order Pages to get")
 	appendAll := flag.Bool("appendAll", false, "append old tenders to new all")
 	tenderOldFileName := flag.String("tenderOldFileName", "przetargi.xlsx", "tender old file name")
 	ordersOldFileName := flag.String("orderOldFileName", "oferty.xlsx", "order old file name")
